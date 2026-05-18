3c6a93cd74bc1e22fec30959ab8c1b5715983d05
ic23b050 <ic23b050@technikum-wien.at>
2026-05-18 13:04:45 +0200

    backend: working spring boot with postgres connection

---
+++ backend/src/main/java/com/anwar/backend/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.anwar.backend.controller;
+
+import com.anwar.backend.entity.Task;
+import com.anwar.backend.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
---
+++ backend/src/main/java/com/anwar/backend/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.anwar.backend.repository;
+
+import com.anwar.backend.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
